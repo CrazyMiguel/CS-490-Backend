eeaaa5ec6e5d5fe8500d7131bfb8f0999c2443a2
CrazyMiguel <55167416+CrazyMiguel@users.noreply.github.com>
2019-10-08 14:50:26 -0400

    Update CS490.php

--- CS490.php
+++ CS490.php
@@ -12,28 +12,34 @@ if ($cnx->connect_error)
 if(isset($_POST['ucid'], $_POST['password']))
 {
 	$ucid = $_POST['ucid'];
-        $password = $_POST['password'];
-	$enteredpass_hashed = hash('sha256', $password);
+        $password = $_POST['password'];		//setting the recieved ucid and password to variables
+	$enteredpass_hashed = hash('sha256', $password); //hashing the input password to compare to hashed password in db
 }
-//else
-//{
-//	echo "FALSE";
-//}
 
-$query1 = "Select password from School where ucid='$ucid'";
+$query1 = "Select password, role from School where ucid='$ucid'";
 $result = mysqli_query($cnx, $query1) or die("BAD QUERY\n");
 while($row = mysqli_fetch_array($result)) 
 {
     $actual_pass=$row['password'];
-}
+    $db_role=$row['role'];
+}					//checking and saving (in a variable) the password for the ucid in the db
 
-if($enteredpass_hashed==$actual_pass)
+if($enteredpass_hashed==$actual_pass) //comparing entered password to the password in db
 {
-	$response = ['response' => 'TRUE'];
-	echo json_encode($response);
+	if($db_role=="professor")
+	{
+		$response = ['response' => 'professor'];
+		echo json_encode($response);	//delivering it in json format {"response":"professor"}
+	}
+	else
+	{
+		$response = ['response' => 'student'];
+		echo json_encode($response);	//delivering it in json format {"response":"student"}
+	}
 }
 else
 {
+	
 	$response = ['response' => 'FALSE'];
 	echo json_encode($response);
 }
