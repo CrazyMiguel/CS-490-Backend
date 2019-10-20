3b61333f4af6f672c87cd87bb11643a5b0c096b6
CrazyMiguel <55167416+CrazyMiguel@users.noreply.github.com>
2019-10-20 16:25:05 -0400

    Exam_name now included

--- Exam.php
+++ Exam.php
@@ -10,11 +10,11 @@ if ($cnx->connect_error)
 	die('Connection failed: ' . $cnx->connect_error);
 
 
-function InsertExamtype()
+function InsertExamtype($Ename)
 {
 	global $cnx;
 
-	$query5 = "INSERT INTO Exams (Testname, Released) VALUES ('python', '0')";
+	$query5 = "INSERT INTO Exams (Testname, Released) VALUES ('$Ename', '0')";
 	$result5 = mysqli_query($cnx, $query5) or die("BAD QUERYs\n");
 }
 
@@ -29,7 +29,8 @@ function InsertQforExam($QID,$EID,$P)
 	//Decoding the JSON file sent via POST
 	$Exam_JSON= json_decode(file_get_contents('php://input'), true);
 	$numquestions=$Exam_JSON["questions"];
-	InsertExamtype();
+	$Examname=$Exam_JSON["exam_name"];
+	InsertExamtype($Examname);
 	$ExamID= intval(mysqli_insert_id($cnx));
 	
 	for($i=0;$i<count($numquestions);$i++)
