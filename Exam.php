fc2dd78d32a07dbf3afb1fe6473d12574c2a7f61
CrazyMiguel <55167416+CrazyMiguel@users.noreply.github.com>
2019-10-20 12:23:28 -0400

    Final Exam version

--- Exam.php
+++ Exam.php
@@ -9,32 +9,35 @@ $cnx = new mysqli($host, $user, $password, $db);
 if ($cnx->connect_error)
 	die('Connection failed: ' . $cnx->connect_error);
 
+
 function InsertExamtype()
 {
-	$query5 = "INSERT INTO Exams (Testname) VALUES ('python')";
-	$result5 = mysqli_query($cnx, $query5) or die("BAD QUERY\n");
+	global $cnx;
+
+	$query5 = "INSERT INTO Exams (Testname, Released) VALUES ('python', '0')";
+	$result5 = mysqli_query($cnx, $query5) or die("BAD QUERYs\n");
 }
 
 function InsertQforExam($QID,$EID,$P)
 {
+	global $cnx;
 	$query6 = "INSERT INTO Creates_with (QuestionID, ExamID, Points) VALUES ('$QID', '$EID', '$P')";
-	$result6 = mysqli_query($cnx, $query6) or die("BAD QUERY\n");
+	$result6 = mysqli_query($cnx, $query6) or die("BAD QUERYm\n");
 }
 
 //Exam Creation
 	//Decoding the JSON file sent via POST
-	$Exam_PHP= json_decode(file_get_contents('php://input'), true)
-	$numquestions=$Exam_PHP["questions"]
+	$Exam_JSON= json_decode(file_get_contents('php://input'), true);
+	$numquestions=$Exam_JSON["questions"];
 	InsertExamtype();
-	$query7= "SELECT ExamID FROM Exams ORDER BY ExamID DESC LIMIT 1"
-	$ExamID = mysqli_query($cnx, $query7) or die("BAD Query\n");
-
-	for($i=0;$i<=count($numquestions);$i++)
+	$ExamID= intval(mysqli_insert_id($cnx));
+	
+	for($i=0;$i<count($numquestions);$i++)
 	{
-		$QuestionID=["questionid"];
-		$Points=["points"];
+		$QuestionID=$numquestions[$i]['questionid'];
+		$Points=$numquestions[$i]['points'];
 		InsertQforExam($QuestionID,$ExamID,$Points);
 	}
-}
+
 mysqli_close($cnx);
 ?>
