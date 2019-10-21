2e756a5e0ddd43551d60581ca741fa54f8fd5a85
CrazyMiguel <55167416+CrazyMiguel@users.noreply.github.com>
2019-10-21 17:53:08 -0400

    made question available in the json

--- sendstudentexam.php
+++ sendstudentexam.php
@@ -13,10 +13,42 @@ if(isset($_POST['studentexam']))
 {
 	$query1 = "Select ExamID from Exams where Released='1'";
 	$result = mysqli_query($cnx, $query1) or die("BAD QUERY\n");
-	echo $result;
-	$query2 = "Select QuestionID, Points from Creates_with where ExamID='$result'";
+
+	while($row = mysqli_fetch_array($result)) 
+	{
+	    $ExamID=$row['ExamID'];
+	}
+	$query2 = "Select QuestionID, Points from Creates_with where ExamID='$ExamID'"; //getting points
 	$result2 = mysqli_query($cnx, $query2) or die("BAD QUERY\n");
+	
+	$exam_arr=array();
+	while($row = mysqli_fetch_array($result2)) 
+	{
+	    $QID=$row['QuestionID'];
+	    $Points=$row['Points'];
+	    $query3 = "Select question from Question_Bank where QuestionID='$QID'";
+	    $result3 = mysqli_query($cnx, $query3) or die("BAD QUERY\n");
+	    while($row = mysqli_fetch_array($result3))
+	    {
+		$question=$row['question'];
+		$newquestion = array("questionid" => $QID,"question" => $question,"Points" => $Points);
+		array_push($exam_arr, $newquestion);
+	    } 
+	}
+	$response = ["questions" => $exam_arr];
+	echo json_encode($response);
 	
 }
 
-?>
+/*
+	{"questions":[{
+		"questionid":"120"
+		"question":"fjklsfjdlksjdflksjd"
+		"points":"20"},
+		{
+		"questionid":"120"
+		"question":"fjklsfjdlksjdflksjd"
+		"points":"20"}]
+
+}
+*/
