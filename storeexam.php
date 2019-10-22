177a4dd704cf37e279e1da8cdf4791e703171cb9
CrazyMiguel <55167416+CrazyMiguel@users.noreply.github.com>
2019-10-22 10:07:14 -0400

    changed testcases to questions

--- storeexam.php
+++ storeexam.php
@@ -15,19 +15,25 @@ function InsertTakenExam($EID,$UCID,$QID,$R,$C,$Sub)
 	$query3 = "INSERT INTO Stored_Exams (ExamID, UCID, QuestionID, Results, Comments, Submission) VALUES ('$EID', '$UCID', '$QID', '$R', '$C','$Sub'))";
 	$result3 = mysqli_query($cnx, $query3) or die("BAD QUERYf\n");
 }
-
+function InsertTakenExam($EID,$UCID)
+{
+	global $cnx;
+	$query4 = "INSERT INTO Stored_Exams (ExamID, UCID) VALUES ('$EID', '$UCID'))";
+	$result4 = mysqli_query($cnx, $query4) or die("BAD QUERYf\n");
+}
 //StoreTakenExam
 	//Decoding the JSON file sent via POST
 	$Question_JSON= json_decode(file_get_contents('php://input'), true); //listening for a Json Post
 	$ucid=$Question_JSON["student"];
 	$examID= $Question_JSON["examid"];
 	$questions=$Question_JSON['questions'];
+	InsertReview($examID,$ucid);
 	for($i=0;$i<count($questions);$i++)
 	{
 		$qid=$questions[$i]['questionid'];
-		$submission=$testcases[$i]['submission'];
+		$submission=$questions[$i]['submission'];
 		$commments=$questions[$i]['comments'];
-		$grade=$testcases[$i]['grade'];
+		$grade=$questions[$i]['grade'];
 		InsertTakenExam($examID,$ucid,$qid,$grade,$comments,$submission);
 	}
  	
