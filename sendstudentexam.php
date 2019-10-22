1bd9dfe427d16ee90db8b6a7011494bdfb71e7a6
CrazyMiguel <55167416+CrazyMiguel@users.noreply.github.com>
2019-10-21 20:10:07 -0400

    Accepting Multiple released exams

--- sendstudentexam.php
+++ sendstudentexam.php
@@ -13,42 +13,39 @@ if(isset($_POST['studentexam']))
 {
 	$query1 = "Select ExamID from Exams where Released='1'";
 	$result = mysqli_query($cnx, $query1) or die("BAD QUERY\n");
-
+	
+	$released_arr=array(); //storing all the exam ids that are released
 	while($row = mysqli_fetch_array($result)) 
 	{
 	    $ExamID=$row['ExamID'];
+	    array_push($released_arr,$ExamID);
 	}
-	$query2 = "Select QuestionID, Points from Creates_with where ExamID='$ExamID'"; //getting points
-	$result2 = mysqli_query($cnx, $query2) or die("BAD QUERY\n");
-	
-	$exam_arr=array();
-	while($row = mysqli_fetch_array($result2)) 
+
+	$exams_arr=array();
+	for($i=0;$i<count($released_arr);$i++)
 	{
-	    $QID=$row['QuestionID'];
-	    $Points=$row['Points'];
-	    $query3 = "Select question from Question_Bank where QuestionID='$QID'";
-	    $result3 = mysqli_query($cnx, $query3) or die("BAD QUERY\n");
-	    while($row = mysqli_fetch_array($result3))
-	    {
-		$question=$row['question'];
-		$newquestion = array("questionid" => $QID,"question" => $question,"Points" => $Points);
-		array_push($exam_arr, $newquestion);
-	    } 
+		$EID=$released_arr[$i];
+		$query2 = "Select QuestionID, Points from Creates_with where ExamID='$EID'"; //getting points
+		$result2 = mysqli_query($cnx, $query2) or die("BAD QUERY\n");
+	
+		$exam_arr=array();
+		while($row = mysqli_fetch_array($result2)) 
+		{
+		    $QID=$row['QuestionID'];
+		    $Points=$row['Points'];
+		    $query3 = "Select question from Question_Bank where QuestionID='$QID'";
+		    $result3 = mysqli_query($cnx, $query3) or die("BAD QUERY\n");
+		    while($row = mysqli_fetch_array($result3))
+		    {
+			$question=$row['question'];
+			$newquestion = array("questionid" => $QID,"question" => $question,"Points" => $Points); //getting the question,id,points, for that speciific exam 
+			array_push($exam_arr, $newquestion);
+	    	    }
+		}
+		$eachexam = array("ExamID" => $EID, "questions" => $exam_arr);	//putting the results of all the released exams into another array in order to encode altogether
+		array_push($exams_arr,$eachexam);
 	}
-	$response = ["questions" => $exam_arr];
-	echo json_encode($response);
+	$exams= ["ExamsReleased" => $exams_arr];
+	echo json_encode($exams);
 	
 }
-
-/*
-	{"questions":[{
-		"questionid":"120"
-		"question":"fjklsfjdlksjdflksjd"
-		"points":"20"},
-		{
-		"questionid":"120"
-		"question":"fjklsfjdlksjdflksjd"
-		"points":"20"}]
-
-}
-*/
