1c73ba1fa2215789a58690ec775cfacc3ad85eeb
CrazyMiguel <55167416+CrazyMiguel@users.noreply.github.com>
2019-11-16 12:48:04 -0500

    added a new aceepting field constraint

--- QuestionBank.php
+++ QuestionBank.php
@@ -9,10 +9,10 @@ $cnx = new mysqli($host, $user, $password, $db);
 if ($cnx->connect_error)
 	die('Connection failed: ' . $cnx->connect_error);
 
-function InsertStuff($Q,$T,$D,$F)
+function InsertStuff($Q,$T,$D,$C,$F)
 {
 	global $cnx;
-	$query3 = "INSERT INTO Question_Bank (question, topic, difficulty, functionname) VALUES ('$Q', '$T', '$D', '$F')";
+	$query3 = "INSERT INTO Question_Bank (question, topic, difficulty, constraint, functionname) VALUES ('$Q', '$T', '$D', '$C', '$F')";
 	$result3 = mysqli_query($cnx, $query3) or die("BAD QUERYf\n");
 }
 
@@ -22,25 +22,18 @@ function InsertTests($QID,$in,$out)
 	$query4 = "INSERT INTO test_cases (QuestionID, in1, out1) VALUES ('$QID', '$in', '$out')";
 	$result4 = mysqli_query($cnx, $query4) or die("BAD QUERYs\n");
 }
-//function GetQuestionID($Q)
-//{	
-//	global $cnx;
-//	$query7= "SELECT QuestionID FROM Question_Bank Where question = '$Q'";
-//	$QID = mysqli_query($cnx, $query7) or die("BAD Querym\n");
-//	return $QID;
-//}
 //QuestionBank
 	//Decoding the JSON file sent via POST
-	$Question_JSON= json_decode(file_get_contents('php://input'), true);
-	$Question_NO= file_get_contents('php://input');
+	$Question_JSON= json_decode(file_get_contents('php://input'), true); //listening for a Json Post
 	$question=$Question_JSON["question"];
 	$topic= $Question_JSON["topic"];
 	$difficulty=$Question_JSON['difficulty'];
+	$constraint=$Question_JSON['constraint'];
 	$functionname=$Question_JSON['functionname'];
 	$testcases=$Question_JSON['testcases'];
 	
 	//Inserting Question into Question Bank
-	InsertStuff($question,$topic,$difficulty,$functionname);
+	InsertStuff($question,$topic,$difficulty,$constraint,$functionname);
 	//GetQuestionID($question);
 	$QID= intval(mysqli_insert_id($cnx));
 	for($i=0;$i<count($testcases);$i++)
